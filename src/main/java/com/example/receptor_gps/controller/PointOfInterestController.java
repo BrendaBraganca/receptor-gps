bdada51f0869fe7847eaa81c633ff2237e774289
brenda_braganca <bragancadeassis@gmail.com>
2025-01-13 12:11:11 -0300

    feat: primeiro endpoint POST

--- src/main/java/com/example/receptor_gps/controller/PointOfInterestController.java
+++ src/main/java/com/example/receptor_gps/controller/PointOfInterestController.java
@@ -1,5 +1,29 @@
 package com.example.receptor_gps.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.receptor_gps.dto.PointOfInterestDTO;
+import com.example.receptor_gps.enitity.PointOfInterest;
+import com.example.receptor_gps.repository.PointOfInterestRepository;
+
+@RestController
 public class PointOfInterestController {
+
+    private final PointOfInterestRepository repository;
+
+    public PointOfInterestController(PointOfInterestRepository repository){
+        this.repository = repository;
+    }
+
+    @PostMapping("/points-of-interests")
+    public ResponseEntity<Void> createPoi(@RequestBody PointOfInterestDTO body){
+
+        repository.save(new PointOfInterest(body.name(), body.x(), body.y())); 
+        
+        return ResponseEntity.ok().build();
+    }
 
 }
