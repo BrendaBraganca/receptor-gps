386d471bbacc1ed3048c1e884c85a523989ab3cb
brenda_braganca <bragancadeassis@gmail.com>
2025-01-13 14:01:32 -0300

    feat: near Me

--- src/main/java/com/example/receptor_gps/repository/PointOfInterestRepository.java
+++ src/main/java/com/example/receptor_gps/repository/PointOfInterestRepository.java
@@ -1,9 +1,18 @@
 package com.example.receptor_gps.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.receptor_gps.enitity.PointOfInterest;
 
 public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long>{
+
+    @Query("""
+        SELECT p FROM PointOfInterest p WHERE (p.x >= :xMin AND p.x <= :xMax AND p.y >= :yMin AND p.y <= :yMax)
+    """)
+    List<PointOfInterest>  findNearMe(@Param("xMin") long xMin,@Param("xMax") long xMax,@Param("yMin") long yMin,@Param("yMax") long yMax);
 
 }
